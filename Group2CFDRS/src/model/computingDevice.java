22dcea43763bd54c37e320968e9b0a9da2800925
dftamayo <dftamayo@windows-11.host-only>
2021-12-10 14:43:20 -0500

    added computing device

---
+++ Group2CFDRS/src/model/computingDevice.java
@@ -0,0 +1,52 @@
+/**
+ * 
+ */
+package model;
+
+/**
+ * @author dftamayo
+ *
+ */
+public class computingDevice {
+
+	private String fileName;
+	private String filePath;
+
+	/**
+	 * @param fileName
+	 * @param filePath
+	 */
+	public computingDevice(String fileName, String filePath) {
+		this.setFileName(fileName);
+		this.setFilePath(filePath);
+	}
+
+	/**
+	 * @return the fileName
+	 */
+	public String getFileName() {
+		return fileName;
+	}
+
+	/**
+	 * @param fileName the fileName to set
+	 */
+	public void setFileName(String fileName) {
+		this.fileName = fileName;
+	}
+
+	/**
+	 * @return the filePath
+	 */
+	public String getFilePath() {
+		return filePath;
+	}
+
+	/**
+	 * @param filePath the filePath to set
+	 */
+	public void setFilePath(String filePath) {
+		this.filePath = filePath;
+	}
+
+}
