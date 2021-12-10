d5ae6976e32fb83ae558e24a12be872e5f160c96
dftamayo <dftamayo@windows-11.host-only>
2021-12-10 14:08:12 -0500

    cfdrs added

---
+++ Group2CFDRS/src/model/cfdrs.java
@@ -0,0 +1,70 @@
+/**
+ * 
+ */
+package model;
+
+/**
+ * @author dftamayo
+ *
+ */
+public class cfdrs {
+
+	private String cfdrsName;
+	private String cfdrsStatus;
+//	private int originalFileSize; <-- possibly delete
+	private String fileOriginalFileSource;
+	private String fileDestination;
+
+	/**
+	 * @param cfdrsName
+	 * @param cfdrsStatus
+	 * @param fileOriginalFileSource
+	 * @param fileDestination
+	 */
+	public cfdrs(String cfdrsName, String cfdrsStatus, String fileOriginalFileSource, String fileDestination) {
+		this.cfdrsName = cfdrsName;
+		this.cfdrsStatus = cfdrsStatus;
+		this.fileOriginalFileSource = fileOriginalFileSource;
+		this.fileDestination = fileDestination;
+	}
+
+	public static void getZippedFileName() {
+		System.out.println("Getting zipped file...");
+	}
+
+	public static void decompressFile() {
+		System.out.println("Decompressing zipped file...");
+	}
+
+	public static void createVirtualEnvironment() {
+		System.out.println("Creating virtual environment...");
+	}
+
+	public static void destroyVirtualEnvironment() {
+		System.out.println("Destroying virtual environment...");
+	}
+
+	public static void checkForMaliciousContent() {
+		System.out.println("Checking files for malicious content...");
+	}
+
+	public static void repackageFile() {
+		System.out.println("Rezipping file...");
+	}
+
+	public static void rerouteFile() {
+		System.out.println("Rerouting zipped file...");
+	}
+
+	public static void moveFile() {
+		System.out.println("Moving file...");
+	}
+
+	public static void copyingFile() {
+		System.out.println("Getting zipped file...");
+	}
+
+	public static void deleteFile() {
+		System.out.println("Deleting file...");
+	}
+}
--- Group2CFDRS/src/model/fileRepo.java
+++ Group2CFDRS/src/model/fileRepo.java
@@ -22,10 +22,80 @@ public class fileRepo {
 	 */
 	public fileRepo(String fileRepoName, int fileRepoCPU, int fileRepoMemory, int fileRepoStorage,
 			String fileRepoPath) {
+		this.setFileRepoName(fileRepoName);
+		this.setFileRepoCPU(fileRepoCPU);
+		this.setFileRepoMemory(fileRepoMemory);
+		this.setFileRepoStorage(fileRepoStorage);
+		this.setFileRepoPath(fileRepoPath);
+	}
+
+	/**
+	 * @return the fileRepoName
+	 */
+	public String getFileRepoName() {
+		return fileRepoName;
+	}
+
+	/**
+	 * @param fileRepoName the fileRepoName to set
+	 */
+	public void setFileRepoName(String fileRepoName) {
 		this.fileRepoName = fileRepoName;
+	}
+
+	/**
+	 * @return the fileRepoCPU
+	 */
+	public int getFileRepoCPU() {
+		return fileRepoCPU;
+	}
+
+	/**
+	 * @param fileRepoCPU the fileRepoCPU to set
+	 */
+	public void setFileRepoCPU(int fileRepoCPU) {
 		this.fileRepoCPU = fileRepoCPU;
+	}
+
+	/**
+	 * @return the fileRepoMemory
+	 */
+	public int getFileRepoMemory() {
+		return fileRepoMemory;
+	}
+
+	/**
+	 * @param fileRepoMemory the fileRepoMemory to set
+	 */
+	public void setFileRepoMemory(int fileRepoMemory) {
 		this.fileRepoMemory = fileRepoMemory;
+	}
+
+	/**
+	 * @return the fileRepoStorage
+	 */
+	public int getFileRepoStorage() {
+		return fileRepoStorage;
+	}
+
+	/**
+	 * @param fileRepoStorage the fileRepoStorage to set
+	 */
+	public void setFileRepoStorage(int fileRepoStorage) {
 		this.fileRepoStorage = fileRepoStorage;
+	}
+
+	/**
+	 * @return the fileRepoPath
+	 */
+	public String getFileRepoPath() {
+		return fileRepoPath;
+	}
+
+	/**
+	 * @param fileRepoPath the fileRepoPath to set
+	 */
+	public void setFileRepoPath(String fileRepoPath) {
 		this.fileRepoPath = fileRepoPath;
 	}
 
